2cd85b3ab9b197c8a44a9b1a0a8b1230f3470250
Anna Malorodova <angervenus6@gmail.com>
2021-02-13 00:11:36 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <img alt="img"  width="200px" src="./assets/mona1.jpg">
+    <h1>Моя первая Vue.js приложуха</h1>
+    <h3>
+     Подкинь монетку Моначке!
+    </h3>
+    <Counter/>
+  </div>
+</template>
+
+<script>
+import Counter from './components/Counter.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Counter
+  }
+}
+</script>
+
+<style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h3 {
+  margin: 40px 0 0;
+  color: blueviolet;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Counter.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <div class="count">{{ count }}</div>
+    <button class="increment" @click="increment">Жми, чтоб прибавить</button>
+    <button class=" decrement" @click="decrement">Жми, чтоб убавить</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Counter',
+  data() {
+    return {
+      // Инициализируем с нуля
+      count: 0,
+    }
+  },
+  methods: {
+    increment() {
+      this.count += 1;
+    },
+    decrement() {
+      this.count -= 1;
+    }
+  }
+}
+</script>
+
+<style scoped>
+button{
+  border-radius: 20%;
+  height: 70px;
+  width: 85px;
+  outline: none;
+  border:none;
+  font-weight: 700;
+  font-family: "Roboto", sans-serif;
+}
+
+.increment{
+  background-color: green;
+}
+
+.decrement{
+  background-color: red;
+  margin-left: 30px;
+}
+
+.count{
+  margin: 20px auto 20px;
+  display: block;
+  width: 50px;
+  font-size: 30px;
+  font-weight: 700;
+  border:2px solid black;
+  padding: 10px;
+}
+</style>
